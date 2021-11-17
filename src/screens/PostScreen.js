997e68a2ca8bfc0956f0feb479aa8038047bad87
rambutan <rambutan@educonlines.com>
2021-11-17 13:05:44 +0200

    added post content && Alert

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,26 +1,63 @@
 import React, {useEffect} from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import {StyleSheet, View, Text, Image, Button, ScrollView, Alert} from 'react-native'
+import {DATA} from '../data'
+import {THEME} from '../theme'
 
 export const PostScreen = ({navigation, route}) => {
   const params = route.params
+  const post = DATA.find(p => p.id === params.postId)
 
   useEffect(() => {
     navigation.setOptions({title: `Post: ${params.postId} on ${new Date(params.date).toLocaleDateString()}`})
   }, [])
 
+  const removeHandler = () => {
+    Alert.alert(
+      "Delete post",
+      "Are you sure?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          onPress: () => {},
+          style: "destructive",
+        },
+      ],
+      {
+        cancelable: false
+      }
+    );
+  }
+
   return (
-    <View style={styles.center}>
-      <Text>
-        PostScreen
-      </Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{uri: post.img}}/>
+      <View style={styles.textWrap}>
+        <Text style={styles.title}>
+          {post.text}
+        </Text>
+      </View>
+      <Button
+        title="Delete"
+        color={THEME.DANGER_COLOR}
+        onPress={removeHandler}
+      />
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+  image: {
+    width: '100%',
+    height: 200
+  },
+  textWrap: {
+    padding: 10
+  },
+  title: {
+    fontFamily: 'open-regular'
+  },
 })
